78fd66e8989da514cbc826108b0457f5406941a7
Andrea D'Amico <andrea.damico@gmail.com>
2009-06-09 12:19:45 +0200

    tweaked css, corrected stylesheet link in app layout

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -2,9 +2,9 @@ class Categoria < ActiveRecord::Base
   has_many :ricette
 
   validates_presence_of :nome, :message => "non puo' essere vuoto"
-  
+
   attr_accessor :raise_exception
-  
+
   after_save :raise_exception_if_needed
   def raise_exception_if_needed
     if @raise_exception.to_i == 1
--- app/models/ricetta.rb
+++ app/models/ricetta.rb
@@ -1,3 +1,13 @@
+# Current Schema
+#create_table "ricette", :force => true do |t|
+#  t.string   "titolo"
+#  t.text     "istruzioni"
+#  t.datetime "created_at"
+#  t.datetime "updated_at"
+#  t.integer  "categoria_id"
+#  t.string   "fonte"
+#end
+
 class Ricetta < ActiveRecord::Base
   validates_presence_of :titolo, :istruzioni
   belongs_to :categoria
